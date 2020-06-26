4e3e0d00a6c101e512949bb9deccc63ce69183b0
Daniel Ruffolo <danielruffolodeveloper@gmail.com>
2020-06-26 11:03:42 +1000

    Create init.sql
    Added sql to create required items table

---
+++ init.sql
@@ -0,0 +1,6 @@
+-- CREATE TABLE IF NOT EXISTS `items` (
+--   id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--   name varchar(255) NOT NULL,
+--   quantity int(10000) NOT NULL,
+--   active BOOLEAN DEFAULT false
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
